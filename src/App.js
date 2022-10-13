f88c2f8fe0df5548fc8716489c58276df5b19402
Mehmet Enfa Vakitsayan <mehmet.enfa@gmail.com>
2022-10-13 13:10:42 +0300

    start

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { languages } from './helper/data.js'
+import reactsvg from './assets/react.svg'
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <img className='reactLogo' src={reactsvg} alt="reactlogo" />
+    <div className='App'>
+      <h2 className='top-h2'>Languages</h2>
+      <div className='main-container'>
+          
+      </div>
     </div>
+    </>
   );
 }
 
