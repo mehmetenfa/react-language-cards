ba0548b0ecd15b3043fe351f7d7f10280a05825e
Mehmet Enfa Vakitsayan <mehmet.enfa@gmail.com>
2022-10-13 14:47:25 +0300

    completed project

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
-import { languages } from './helper/data.js'
-import reactsvg from './assets/react.svg'
+import { languages } from "./helper/data.js";
 import "./App.css";
+import Card from "./components/Card.js";
+import reactsvg from "./assets/react.svg";
 
 function App() {
   return (
     <>
-    <img className='reactLogo' src={reactsvg} alt="reactlogo" />
-    <div className='App'>
-      <h2 className='top-h2'>Languages</h2>
-      <div className='main-container'>
-          
+      <img className="reactlogo" src={reactsvg} alt="react-logo" />
+      <div className="down-container">
+        <div>
+          <h2>Languages</h2>
+          <div className="main-container">
+            {languages.map((language, index) => {
+              return (
+                <div className="container">
+                  <Card key={index} {...language} />
+                </div>
+              );
+            })}
+          </div>
+        </div>
       </div>
-    </div>
     </>
   );
 }
